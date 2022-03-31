e1bd906dee723283768caf3f71776cae10b57340
Beldine-Moturi <beldinemoturi@gmail.com>
2022-03-31 15:11:58 +0300

    use puppet to configure nginx server

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,71 @@
+# installs and configures Nginx server
+
+# update apt-get first
+exec { 'apt-get update':
+  command  => 'sudo apt-get update',
+  provider => 'shell',
+}
+
+# install Nginx package
+package { 'install nginx':
+  ensure => 'present',
+  name   => 'nginx',
+}
+
+# adjust the firewall
+exec { 'allow HTTP':
+  command => "sudo ufw allow 'Nginx HTTP'",
+}
+
+# file permissions and ownership
+exec { 'change file permissions':
+  command => 'sudo chmod -R 755 /var/www',
+}
+
+# change index file
+file { '/var/www/html/index.html':
+  ensure  => 'present',
+  content => "Hello World!\n",
+}
+
+# create custom 404 page
+file { '/var/www/html/custom_404.html':
+  content => "Ceci n'est pas une page\n",
+}
+
+# configure nginx:
+#   lsiten on port 80
+#   redirect /redirect_me to 301
+#   custom 404 page
+file { 'Nginx default config file':
+  ensure  => file,
+  path    => '/etc/nginx/sites-enabled/default',
+  content =>
+"server {
+	listen 80 default_server;
+        listen [::]:80 default_server;
+               root /var/www/html;
+        # Add index.php to the list if you are using PHP
+        index index.html index.htm index.nginx-debian.html;
+        server_name _;
+        location / {
+                # First attempt to serve request as file, then
+                # as directory, then fall back to displaying a 404.
+                try_files \$uri \$uri/ =404;
+        }
+        error_page 404 /custom_404.html;
+        location  /custom_404.html {
+            internal;
+        }
+        
+        location /redirect_me {
+		 rewrite ^/redirect_me$ https://github.com/Beldine-Moturi permanent;
+	}
+}
+",
+}
+
+# restart nginx
+exec { 'restart nginx':
+  command => 'sudo service nginx restart',
+}

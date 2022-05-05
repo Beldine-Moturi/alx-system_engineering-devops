32d1838b5782f0cc44f14968b21d25634126854b
Beldine-Moturi <beldinemoturi@gmail.com>
2022-05-05 21:00:10 +0300

    fix errors

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,22 +1,23 @@
 #!/usr/bin/python3
-"""Defines a function that queries the Reddit API and returns
-the number of subscribers for a given subreddit."""
+"""
+Defines a function that queries the Reddit API and returns
+the number of subscribers for a given subreddit.
+"""
+
 import requests
-import sys
 
 
 def number_of_subscribers(subreddit):
     """Queries the Reddit API"""
-    try:
-        timeframe = 'all'
-        limit = ''
-        base_url = f'https://www.reddit.com/r/{subreddit}/about.json?\
-t={timeframe}'
-        request = requests.get(
-            base_url,
-            headers={'User-agent': 'MyBot'},
-            allow_redirects=False
-        )
+    if subreddit is None or type(subreddit) is not str:
+        return 0
+    timeframe = 'all'
+    base_url = f'https://www.reddit.com/r/{subreddit}/about.json?t={timeframe}'
+    request = requests.get(
+        base_url,
+        headers={'User-agent': 'MyBot'},
+        allow_redirects=False
+    )
     if 'error' in request.json().keys():
         return 0
-    return request.json()['data']['subscribers']
+    return request.json().get("data", {}).get("subscribers", 0)

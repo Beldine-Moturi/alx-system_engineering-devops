5ad9611227c688a959bebb91dd80f987899180af
Beldine-Moturi <beldinemoturi@gmail.com>
2022-05-05 22:43:02 +0300

    fetch top 10 hot posts fora subreddit

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Defines function to query first 10 hot posts for a subreddit
+"""
+import requests
+
+
+def top_ten(subreddit):
+    """prints titles of the first 10 hot posts listed for a given subreddit."""
+    sub = subreddit
+    tf = "all"
+    lim = 10
+
+    base_url = f'https://www.reddit.com/r/{sub}/hot.json?limit={lim}&t={tf}'
+    request = requests.get(
+        base_url,
+        headers={'User-agent': 'MyBot'},
+        allow_redirects=False
+    )
+    if request.status_code == 404:
+        print("None")
+        return
+    r = request.json()
+    [print(p.get('data').get('title')) for p in r.get('data').get('children')]

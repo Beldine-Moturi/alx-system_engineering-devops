cb9734a038ee4fbd11795a393c58b949f91da488
Beldine-Moturi <beldinemoturi@gmail.com>
2022-05-05 10:18:22 +0300

    export data from a REST API to csv format

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""uses a REST API and for a given employee ID, returns information
+about his/her TODO list progress."""
+import csv
+import requests
+import sys
+
+
+API = 'https://jsonplaceholder.typicode.com'
+
+
+if __name__ == "__main__":
+    id = sys.argv[1]
+    user = requests.get('{}/users/{}'.format(API, id)).json()
+    tasks = requests.get('{}/users/{}/todos'.format(API, id)).json()
+
+    with open(f'{id}.csv', 'w', newline='') as f:
+        writer = csv.DictWriter(
+            f,
+            fieldnames=["userId", "username", "completed", "title"],
+            quoting=csv.QUOTE_ALL
+        )
+        for task in tasks:
+            writer.writerow({
+                "userId": id,
+                "username": user.get('name'),
+                "completed": task.get('completed'),
+                "title": task.get('title')
+            })

0773c149f968cdea720c4b7c0de7fa5ca1667c78
Beldine-Moturi <beldinemoturi@gmail.com>
2022-05-06 16:14:48 +0300

    make file executable

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,11 +1,11 @@
-# fixes nginx faile requests by increasing ulimit
-exec { 'increase ulimit':
+# fixes nginx failes requests by increasing the ulimit value
+exec { 'modify_file':
   command => "sed -i 's|ULIMIT=\"-n ..\"|ULIMIT=\"-n 2000\"|g' /etc/default/nginx",
   path    => '/bin',
   notify  => Exec['Restart'],
 }
 
-exec { 'restart nginx':
+exec { 'Restart':
   command     => 'service nginx restart',
   path        => '/usr/bin',
   refreshonly => true,
